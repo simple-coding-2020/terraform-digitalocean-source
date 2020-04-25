7f46e10b5a4857844d6ae75df933140263c403a7
Simple Coding <simple.coding.2020@gmail.com>
2020-04-25 15:15:00 +0200

    Adding firewalls

--- main.tf
+++ main.tf
@@ -58,6 +58,46 @@ resource "digitalocean_loadbalancer" "web" {
   droplet_ids            = digitalocean_droplet.web.*.id
 }
 
+resource "digitalocean_firewall" "web" {
+  name = "web-droplet-firewall"
+
+  droplet_ids = digitalocean_droplet.web.*.id
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol                  = "tcp"
+    port_range                = "8080"
+    source_load_balancer_uids = [digitalocean_loadbalancer.web.id]
+  }
+
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
 resource "digitalocean_domain" "domain" {
   name = "simple-coding.co.uk"
 }
